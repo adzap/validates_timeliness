b8283a7c9c1ab75730ebf67268147661e183f115
Adam Meehan <adam.meehan@gmail.com>
2008-05-03 08:12:44 +1000

    outputs AR version being tested

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,17 +4,20 @@ require 'rubygems'
 require 'spec'
 
 if File.exists?(File.dirname(__FILE__) + '/../../../rails')
+  $: << File.dirname(__FILE__) + '/../../../rails'
   require 'activerecord/lib/active_record'
   require 'activerecord/lib/active_record/version' 
 else  
   require 'active_record'
   require 'active_record/version'
 end
+puts "Using ActiveRecord version #{ActiveRecord::VERSION::STRING}"
+
 
 require 'validates_timeliness'
 
 conn = {
-  :adapter  => 'sqlite3',  
+  :adapter  => 'sqlite3',
   :database  => ':memory:'
 }
 
