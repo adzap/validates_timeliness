10856b32d0317b3d183ee221d441216a942bb5e6
Adam Meehan <adam.meehan@gmail.com>
2008-05-14 11:22:44 +1000

    added vendored_rails check for time zone setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,16 +9,19 @@ if File.exists?(File.dirname(__FILE__) + '/../../../../vendor/rails')
   require 'activerecord/lib/active_record'  
   require 'railties/lib/rails/version'  
   
+  vendored_rails = true  
   puts "Using vendored Rails version #{Rails::VERSION::STRING}"
 else  
   require 'active_record'
   require 'active_record/version'
   require 'rails/version'
   
+  vendored_rails = false
   puts "Using gem Rails version #{Rails::VERSION::STRING}"
 end
 
-Time.zone_default = Time.send!(:get_zone, 'UTC')
+
+Time.zone_default = Time.send!(:get_zone, 'UTC') if vendored_rails
 
 require 'validates_timeliness'
 
