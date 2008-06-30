8b69ec81341d4a4f9e4ddd5e784976fa1edf6b35
Adam Meehan <adam.meehan@gmail.com>
2008-06-30 22:37:51 +1000

    changed schema to be version agnostic

--- spec/resources/schema.rb
+++ spec/resources/schema.rb
@@ -1,9 +1,9 @@
 ActiveRecord::Schema.define(:version => 1) do
 
   create_table "people", :force => true do |t|    
-    t.string   "name"
-    t.datetime "birth_date_and_time"
-    t.date "birth_date"
+    t.column "name", :string
+    t.column "birth_date_and_time", :datetime
+    t.column "birth_date", :date
   end
   
 end
