68463908be51231e514580945ddcf0d70cd4be8f
Adam Meehan <adam.meehan@gmail.com>
2008-07-07 09:57:09 +1000

    changed require and include for InstanceTag

--- lib/validates_timeliness.rb
+++ lib/validates_timeliness.rb
@@ -1,8 +1,10 @@
 require 'validates_timeliness/base'
 require 'validates_timeliness/attribute_methods'
 require 'validates_timeliness/validations'
-require 'validates_timeliness/validate_timeliness_matcher'
+require 'validates_timeliness/instance_tag'
+#require 'validates_timeliness/validate_timeliness_matcher'
 
 ActiveRecord::Base.send(:include, ValidatesTimeliness::Base)
 ActiveRecord::Base.send(:include, ValidatesTimeliness::AttributeMethods)
 ActiveRecord::Base.send(:include, ValidatesTimeliness::Validations)
+ActionView::Helpers::InstanceTag.send(:include, ValidatesTimeliness::InstanceTag)
