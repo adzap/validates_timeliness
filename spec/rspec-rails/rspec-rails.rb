a5daef2069e569bd8896ecf5658163834280408c
Adam Meehan <adam.meehan@gmail.com>
2008-07-07 15:17:11 +1000

    added library require file for rspec-rails

---
+++ spec/rspec-rails/rspec-rails.rb
@@ -0,0 +1,8 @@
+module Spec; module Rails; module Mocks; end; end; end
+class ApplicationController; end
+
+require 'rspec-rails/object'
+require 'rspec-rails/assert_select'
+require 'rspec-rails/rails_example_group'
+require 'rspec-rails/functional_example_group'
+require 'rspec-rails/helper_example_group'
