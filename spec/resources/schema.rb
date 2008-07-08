8c5b37271657bf6626a38b2657eb28f5272c96c4
Adam Meehan <adam.meehan@gmail.com>
2008-07-08 10:38:14 +1000

    added birth_time attribute

--- spec/resources/schema.rb
+++ spec/resources/schema.rb
@@ -4,6 +4,7 @@ ActiveRecord::Schema.define(:version => 1) do
     t.column "name", :string
     t.column "birth_date_and_time", :datetime
     t.column "birth_date", :date
+    t.column "birth_time", :time
   end
   
 end
