899d420b5966b67ac162e45249f3e67a9bec4d88
Adam Meehan <adam.meehan@gmail.com>
2008-07-11 16:35:52 +1000

    renamed base to multiparameter attributes

--- lib/validates_timeliness.rb
+++ lib/validates_timeliness.rb
@@ -1,6 +1,6 @@
-require 'validates_timeliness/base'
 require 'validates_timeliness/attribute_methods'
 require 'validates_timeliness/validations'
+require 'validates_timeliness/multiparameter_attributes'
 require 'validates_timeliness/instance_tag'
 require 'validates_timeliness/validate_timeliness_matcher' if ENV['RAILS_ENV'] == 'test'
 
@@ -8,9 +8,9 @@ require 'validates_timeliness/core_ext/time'
 require 'validates_timeliness/core_ext/date'
 require 'validates_timeliness/core_ext/date_time'
 
-ActiveRecord::Base.send(:include, ValidatesTimeliness::Base)
 ActiveRecord::Base.send(:include, ValidatesTimeliness::AttributeMethods)
 ActiveRecord::Base.send(:include, ValidatesTimeliness::Validations)
+ActiveRecord::Base.send(:include, ValidatesTimeliness::MultiparameterAttributes)
 ActionView::Helpers::InstanceTag.send(:include, ValidatesTimeliness::InstanceTag)
 
 Time.send(:include, ValidatesTimeliness::CoreExtensions::Time)
