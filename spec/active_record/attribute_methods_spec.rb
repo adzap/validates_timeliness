19457a6c1d2fca4d6a6b701617801b2c41616d66
Adam Meehan <adam.meehan@gmail.com>
2009-02-27 23:40:14 +1100

    removed some module inclusion silliness in spec

--- spec/active_record/attribute_methods_spec.rb
+++ spec/active_record/attribute_methods_spec.rb
@@ -1,9 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ValidatesTimeliness::ActiveRecord::AttributeMethods do
-  include ValidatesTimeliness::ActiveRecord::AttributeMethods
-  include ValidatesTimeliness::ValidationMethods
-  
   before do
     @person = Person.new
   end
