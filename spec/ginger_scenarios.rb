3bfc7b748f77693b68fe67532bf66e6002d54a57
Adam Meehan <adam.meehan@gmail.com>
2009-12-11 15:19:07 +1100

    more rails versions in ginger scenarios

--- spec/ginger_scenarios.rb
+++ spec/ginger_scenarios.rb
@@ -1,15 +1,15 @@
 # For use with the ginger gem to test plugin against multiple versions of Rails.
 #
 # To use ginger:
-#    
-#    sudo gem install freelancing-god-ginger --source=http://gems.github.com
+#
+#   gem install ginger
 #
 # Then run
 #
 #   ginger spec
 #
 Ginger.configure do |config|
-  rails_versions = ['2.0.2', '2.1.2', '2.2.2', '2.3.3', '2.3.4']
+  rails_versions = ['2.0.2', '2.1.2', '2.2.2', '2.3.3', '2.3.4', '2.3.5']
 
   rails_versions.each do |v|
     g = Ginger::Scenario.new("Rails #{v}")
