6301d87829d9d73fd2f1b3b617199ceba4f3c4b8
Adam Meehan <adam.meehan@gmail.com>
2010-10-11 12:46:53 +1100

    fix uncommented setting in config template

--- lib/generators/validates_timeliness/templates/validates_timeliness.rb
+++ lib/generators/validates_timeliness/templates/validates_timeliness.rb
@@ -3,7 +3,7 @@ ValidatesTimeliness.setup do |config|
   # config.extend_orms = [ :active_record ]
   #
   # Default timezone
-  config.default_timezone = :utc
+  # config.default_timezone = :utc
   #
   # Set the dummy date part for a time type values.
   # config.dummy_date_for_time_type = [ 2000, 1, 1 ]
