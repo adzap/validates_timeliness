aad2db8662e4b517a31f5dda083ccd591c42281d
Adam Meehan <adam.meehan@gmail.com>
2012-06-23 18:58:33 +1000

    Remove unused cargo culted generator method

--- lib/generators/validates_timeliness/install_generator.rb
+++ lib/generators/validates_timeliness/install_generator.rb
@@ -3,7 +3,6 @@ module ValidatesTimeliness
     class InstallGenerator < Rails::Generators::Base
       desc "Copy ValidatesTimeliness default files"
       source_root File.expand_path('../templates', __FILE__)
-      class_option :template_engine
 
       def copy_initializers
         copy_file 'validates_timeliness.rb', 'config/initializers/validates_timeliness.rb'
