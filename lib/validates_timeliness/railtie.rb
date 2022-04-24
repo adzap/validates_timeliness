71b1a437a7f109dc5d122103942783f97687016f
Peter Goldstein <peter.m.goldstein@gmail.com>
2022-04-24 14:58:35 -0700

    Fix deprecation warning

--- lib/validates_timeliness/railtie.rb
+++ lib/validates_timeliness/railtie.rb
@@ -1,8 +1,9 @@
 module ValidatesTimeliness
   class Railtie < Rails::Railtie
-    initializer "validates_timeliness.initialize_active_record", :after => 'active_record.initialize_timezone' do
+    initializer "validates_timeliness.initialize_active_record", after: "active_record.initialize_timezone" do
       ActiveSupport.on_load(:active_record) do
-        ValidatesTimeliness.default_timezone = ActiveRecord::Base.default_timezone
+        ValidatesTimeliness.default_timezone =
+          ActiveRecord.respond_to?(:default_timezone) ? ActiveRecord.default_timezone : ActiveRecord::Base.default_timezone
         ValidatesTimeliness.extend_orms << :active_record
         ValidatesTimeliness.load_orms
       end
@@ -12,7 +13,8 @@ module ValidatesTimeliness
       ValidatesTimeliness.ignore_restriction_errors = !Rails.env.test?
     end
 
-    initializer "validates_timeliness.initialize_timeliness_ambiguous_date_format", :after => :load_config_initializers do
+    initializer "validates_timeliness.initialize_timeliness_ambiguous_date_format",
+                after: :load_config_initializers do
       if Timeliness.respond_to?(:ambiguous_date_format) # i.e. v0.4+
         # Set default for each new thread if you have changed the default using
         # the format switching methods.
